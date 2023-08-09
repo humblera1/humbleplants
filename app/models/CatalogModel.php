25e18357b062c1ff0c39059500480bed6b861499
humblerat <humblerat@mail.ru>
2023-08-09 23:10:35 +0700

    Черновой вариант фильтрации каталога

--- app/models/CatalogModel.php
+++ app/models/CatalogModel.php
@@ -36,4 +36,88 @@ class CatalogModel extends Model
 
         return $this->data;
     }
+
+    public function getWithFilters($filters){
+        
+        $query = 'SELECT id, name, latin_name, short_description, category FROM catalog';
+        $where = '';
+        $params = [];
+
+        $categories = $filters['category'];
+
+        $placeholders = '?';     
+        for ($i=0; $i<count($categories)-1; $i++){
+            $placeholders .= ',?';
+        }
+        
+
+        
+        $whereCategory = '';
+        if (!empty($categories)){
+            $whereCategory = 'WHERE category IN ()';
+            $pos = strpos($whereCategory, '(') + 1;
+            $placeholders = '?';
+            for ($i=0; $i<count($categories)-1; $i++){
+                $placeholders .= ',?';
+            }
+            $whereCategory = substr_replace($whereCategory, $placeholders, $pos, 0);
+            $where .= $whereCategory;
+            $params = array_merge($params, $categories);
+            
+        }
+        
+
+        if (isset($filters['light'])) $light = $filters['light'];
+        $whereLight = '';
+        if (!empty($light)){
+            if (empty($where)){
+                $whereLight = 'WHERE light = ?';
+            }else{
+                $whereLight = ' AND light = ?';
+            }
+            $where .= $whereLight;
+            $params[] = $light;
+        }
+
+        if (isset($filters['watering'])) $watering = $filters['watering'];
+        $whereWatering = '';
+        if (!empty($watering)){
+            if (empty($where)){
+                $whereWatering = 'WHERE watering = ?';
+            }else{
+                $whereWatering = ' AND watering = ?';
+            }
+            $where .= $whereWatering;
+            $params[] = $watering;
+        }
+
+        if (isset($filters['difficulty'])) $difficulty = $filters['difficulty'];
+        $whereDifficulty = '';
+        if (!empty($difficulty)){
+            if (empty($where)){
+                $whereDifficulty = 'WHERE difficulty = ?';
+            }else{
+                $whereDifficulty = ' AND difficulty = ?';
+            }
+            $where .= $whereDifficulty;
+            $params[] = $difficulty;
+        }
+
+        
+ 
+        $query .= ' ' . $where . ' ' . 'ORDER BY category ASC';
+        $statement = $this->db->prepare($query);
+        $statement->execute($params);
+        $this->data = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return ($this->data);
+
+
+
+
+        
+
+        
+        // return $this->data;
+
+    }
 }
